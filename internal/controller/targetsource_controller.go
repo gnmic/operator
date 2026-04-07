26669868c647484ee389232a6babf8adc315d2f8
Valentino Diller <valentino@diller.gl>
2026-04-06 21:01:09 -0600

    added comments

--- internal/controller/targetsource_controller.go
+++ internal/controller/targetsource_controller.go
@@ -55,25 +55,28 @@ func (r *TargetSourceReconciler) Reconcile(ctx context.Context, req ctrl.Request
 
 	// VD: Approach for the reconciliation loop:
 	// 1. Fetch objects from TargetSource
-	// 2. Build desired state
-	// 3. Get actual state (only targets owned by TargetSource)
-	// 4. Compute diff
-	// 5. Apply changes (create, update, delete)
+	// 2. Get actual state (only targets owned by TargetSource)
+	// 3. Compute diff
+	// 4. Apply changes (create, update, delete)
 
+	// Step 1: Get desired state from discovery source
 	discoveredTargets, err := discovery.FetchDiscoveryTargets(ctx, targetSource)
 	if err != nil {
 		logger.Error(err, "error getting discovered targets")
 		return ctrl.Result{}, err
 	}
 
+	// Step 2: Get current state from Kubernetes cluster (lookup by label of TargetSource)
 	existingTargets, err := discovery.FetchExistingTargets(ctx, r.Client, targetSource)
 	if err != nil {
 		logger.Error(err, "error fetching existing targets")
 		return ctrl.Result{}, err
 	}
 
+	// Step 3: Compute diff
 	diff := discovery.BuildDiff(existingTargets, discoveredTargets)
 
+	// Step 4: Iterate over each list and do create, update, delete respectively
 	for _, t := range diff.ToCreate {
 		err = controllerutil.SetControllerReference(&targetSource, &t, r.Scheme)
 		if err != nil {
--- internal/discovery/mapper.go
+++ internal/discovery/mapper.go
@@ -26,18 +26,22 @@ func BuildDiff(existing, discovered []gnmicv1alpha1.Target) Diff {
 		discoveredMap[key] = e
 	}
 
+	// Loop for targets to create + update
 	for _, t := range discovered {
 		key := t.Namespace + "/" + t.Name
 
+		// Check if target already exists
 		if e, found := existingMap[key]; found {
+			// Check if the spec of the target changed
 			if !equality.Semantic.DeepEqual(e.Spec, t.Spec) {
 				diff.ToUpdate = append(diff.ToUpdate, t)
 			}
-		} else {
+		} else { // Target is new
 			diff.ToCreate = append(diff.ToCreate, t)
 		}
 	}
 
+	// Loop for targets to delete
 	for _, e := range existing {
 		key := e.Namespace + "/" + e.Name
 
