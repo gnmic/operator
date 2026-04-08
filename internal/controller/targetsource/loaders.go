3f4832a06a164d640b6cd781e7f85bd7bd44da3b
Daniel Schatzmann <daniel.schatzmann@ost.ch>
2026-04-08 15:00:39 +0000

    init skeleton for target autodiscover implementation

---
+++ internal/controller/targetsource/loaders.go
@@ -0,0 +1,3 @@
+package targetsource
+// This file defines the loader interface
+// targets received from loaders are sent via channel to the controller for reconciliation
---
+++ internal/controller/targetsource/loaders/http_pull/loader.go
@@ -0,0 +1,2 @@
+package http_pull
+// this file implements the logic to load targets from HTTP endpoint
