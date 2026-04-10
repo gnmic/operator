edcda75b79b19a0995294efcee7274cf9469e6a3
Daniel Schatzmann <daniel.schatzmann@ost.ch>
2026-04-10 12:54:33 +0000

    added type for discovered targets

---
+++ internal/controller/targetsource/types.go
@@ -0,0 +1,9 @@
+package targetsource
+
+// DiscoveredTarget represents a target discovered from an external source
+// before it is materialized as a Kubernetes Target CR
+type DiscoveredTarget struct {
+	Name    string
+	Address string
+	Labels  map[string]string
+}
