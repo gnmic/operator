4cab2ba106e72199d78f052ebeea0329d77f6d6c
Valentino Diller <valentino@diller.gl>
2026-04-13 15:14:06 -0600

    added discovery message to types

--- internal/controller/targetsource/types.go
+++ internal/controller/targetsource/types.go
@@ -10,6 +10,19 @@ type DiscoveredTarget struct {
 	Labels  map[string]string
 }
 
+const (
+	DELETE DiscoveryEvent = 0
+	CREATE DiscoveryEvent = 1
+	UPDATE DiscoveryEvent = 2
+)
+
+type DiscoveryEvent int
+
+type DiscoveryMessage struct {
+	Target DiscoveredTarget
+	Event  DiscoveryEvent
+}
+
 // TargetManager consumes discovered targets and applies them to Kubernetes.
 type TargetManager struct {
 	client       client.Client
