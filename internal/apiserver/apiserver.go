f683a44cb7e441c23e554f486034deb53b6c4a09
Janooski <mail@romanweber.ch>
2026-05-04 09:06:52 +0000

    comments from weekly

--- internal/apiserver/apiserver.go
+++ internal/apiserver/apiserver.go
@@ -4,6 +4,8 @@ package apiserver
 // To generate code, install openapi-codegen from https://github.com/oapi-codegen/oapi-codegen)
 // Then use: go generate ./internal/apiserver
 
+// kubectl port-forward -n gnmic-system svc/gnmic-controller-manager-api 8082:8082 --address=0.0.0.0
+
 import (
 	"context"
 	"net/http"
@@ -40,12 +42,11 @@ func New(addr string, clusterReconciler *controller.ClusterReconciler, chunkSize
 		clusterReconciler: clusterReconciler,
 		chunkSize:         chunkSize,
 	}
+	// /api/v1/:namespace/target-source/:target_source_name
 	apiBaseURL := "/api/v1/:namespace/:gNMIcControllerName"
 	RegisterHandlersWithOptions(router, a, GinServerOptions{BaseURL: apiBaseURL})
 	return a, nil
 }
-
-// kubectl port-forward -n gnmic-system svc/gnmic-controller-manager-api 8082:8082 --address=0.0.0.0
 
 // GetClusterPlan returns cluster plan
 func (a *APIServer) GetClusterPlan(c *gin.Context) {
@@ -74,86 +75,10 @@ func (a *APIServer) CreateTargets(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "TargetSource " + payloadTargets.TargetSourceNameSpace + " / " + payloadTargets.TargetSourceName + " does not exist"})
 		return
 	}
-
+	// make sure channel is not closed if targetsource in registry is deleted
+	// timeout for sending to the channel
 	targets := createDiscoveryEvent(payloadTargets)
 	// fmt.Printf("core.DiscoveryEvent was created: %v", targets)
 	core.SendEvents(context.Background(), ch, targets, a.chunkSize)
 	c.JSON(http.StatusOK, payloadTargets)
-}
-
-// createDiscoveryEvent creates object of type core.DiscoveryEvent
-func createDiscoveryEvent(payloadTargets Targets) []core.DiscoveryEvent {
-	targets := []core.DiscoveryEvent{}
-	if len(payloadTargets.TargetList) > 0 {
-		for i, target := range payloadTargets.TargetList {
-			if target.Address == "" || target.Name == "" || target.Operation == "" {
-				// no REST API return here as not all targets might
-				logger.Warn("Target receieved at index", i, " by pull interface does not contain Address, Name or Operation and is skipped.")
-				break
-			}
-			if target.Operation.Valid() != true {
-				logger.Warn("Target receieved at index", i, " by pull interface has invalid Operation.")
-				break
-			}
-
-			targets = append(targets, core.DiscoveryEvent{
-				Target: core.DiscoveredTarget{
-					Name:    target.Name,
-					Address: target.Address,
-					Labels:  convertTargetLabelsToMap(target),
-				},
-				Event: getEvent(target),
-			})
-		}
-	}
-	return targets
-}
-
-// getKey returns key for used to identify correct channel in DiscoveryRegistry
-func getKey(payloadTargets Targets) types.NamespacedName {
-	key := types.NamespacedName{
-		Namespace: payloadTargets.TargetSourceNameSpace,
-		Name:      payloadTargets.TargetSourceName,
-	}
-	return key
-}
-
-// convertTargetLabelsToMap converts target.Labels to map.
-func convertTargetLabelsToMap(target Target) map[string]string {
-	labelToMap := make(map[string]string)
-	if target.Labels != nil {
-		for _, tag := range *target.Labels {
-			if tag.Key == nil || tag.Value == nil || *tag.Key == "" {
-				continue
-			}
-			labelToMap[*tag.Key] = *tag.Value
-		}
-	}
-	return labelToMap
-}
-
-// getEvent converts target.Operation to core.Operation.
-func getEvent(target Target) core.EventAction {
-	event := core.CREATE
-	switch target.Operation {
-	case Created:
-		event = core.UPDATE
-	case Updated:
-		event = core.UPDATE
-	case Deleted:
-		event = core.DELETE
-	default:
-		logger.Warn("Received invalid Operation flag")
-	}
-	return event
-}
-
-// parseURI parses URI to urlStruct.
-func parseURI(c *gin.Context) (url urlStruct) {
-	var u urlStruct
-	if err := c.ShouldBindUri(&u); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	return u
 }
---
+++ internal/apiserver/helpers.go
@@ -0,0 +1,91 @@
+package apiserver
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/bytedance/gopkg/util/logger"
+	"github.com/gin-gonic/gin"
+	"github.com/gnmic/operator/internal/controller/discovery/core"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+// createDiscoveryEvent creates object of type core.DiscoveryEvent
+func createDiscoveryEvent(payloadTargets Targets) []core.DiscoveryEvent {
+	targets := []core.DiscoveryEvent{}
+	if len(payloadTargets.TargetList) > 0 {
+		for i, target := range payloadTargets.TargetList {
+			if target.Name == "" {
+				// no REST API return here as not all targets might be incomplete
+				err := fmt.Errorf("Target receieved at index %d by pull interface has no Name and is skipped.", i)
+				logger.Error(err, "Failed creating DiscoveryEvent")
+				break
+			}
+			if target.Address == "" {
+				err := fmt.Errorf("Target receieved at index %d by pull interface has no Address and is skipped.", i)
+				logger.Error(err, "Failed creating DiscoveryEvent")
+				break
+			}
+
+			targets = append(targets, core.DiscoveryEvent{
+				Target: core.DiscoveredTarget{
+					Name:    target.Name,
+					Address: target.Address,
+					Labels:  convertTargetLabelsToMap(target),
+				},
+				Event: getEvent(target, i),
+			})
+		}
+	}
+	return targets
+}
+
+// getKey returns key for used to identify correct channel in DiscoveryRegistry
+func getKey(payloadTargets Targets) types.NamespacedName {
+	key := types.NamespacedName{
+		Namespace: payloadTargets.TargetSourceNameSpace,
+		Name:      payloadTargets.TargetSourceName,
+	}
+	return key
+}
+
+// convertTargetLabelsToMap converts target.Labels to map.
+func convertTargetLabelsToMap(target Target) map[string]string {
+	labelToMap := make(map[string]string)
+	if target.Labels != nil {
+		for _, tag := range *target.Labels {
+			if tag.Key == nil || tag.Value == nil || *tag.Key == "" {
+				continue
+			}
+			labelToMap[*tag.Key] = *tag.Value
+		}
+	}
+	return labelToMap
+}
+
+// getEvent converts target.Operation to core.Operation.
+func getEvent(target Target, index int) core.EventAction {
+	event := core.CREATE
+	switch target.Operation {
+	case Created:
+		event = core.UPDATE
+	case Updated:
+		event = core.UPDATE
+	case Deleted:
+		event = core.DELETE
+	default:
+		err := fmt.Errorf("Target receieved at index %d by pull interface has no valid Operation and is skipped.", index)
+		logger.Error(err, "Failed creating DiscoveryEvent")
+	}
+	return event
+}
+
+// parseURI parses URI to urlStruct.
+func parseURI(c *gin.Context) (url urlStruct) {
+	var u urlStruct
+	if err := c.ShouldBindUri(&u); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	return u
+}
