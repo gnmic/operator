6c82320ff05d704910675eb011e6e05c882b0d4a
Daniel Schatzmann <daniel.schatzmann@ost.ch>
2026-05-06 08:27:17 +0000

    refactor

--- api/v1alpha1/targetsource_types.go
+++ api/v1alpha1/targetsource_types.go
@@ -97,8 +97,8 @@ type TokenAuthSpec struct {
 }
 
 // +kubebuilder:validation:XValidation:rule="!((has(self.token) || has(self.tokenSecretRef)) && (has(self.signingKeySecretRef) || has(self.claims)))",message="static JWT token and generated JWT configuration cannot be combined"
-// +kubebuilder:validation:XValidation:rule="!( (has(self.token) || has(self.tokenSecretRef)) && (has(self.signingKeySecretRef) || has(self.claims)) )",message="static JWT token and generated JWT configuration cannot be combined"
-// +kubebuilder:validation:XValidation:rule="!has(self.signingKeySecretRef) || self.algorithm != ”",message="algorithm must be specified when generating a JWT"
+// +kubebuilder:validation:XValidation:rule="!((has(self.token) || has(self.tokenSecretRef)) && (has(self.signingKeySecretRef) || has(self.claims)))",message="static JWT token and generated JWT configuration cannot be combined"
+// +kubebuilder:validation:XValidation:rule="!has(self.signingKeySecretRef) || self.algorithm != \"\"",message="algorithm must be specified when generating a JWT"
 type JWTAuthSpec struct {
 	// Static pre-generated JWT
 	Token          string                    `json:"token,omitempty"`
