209948e2e1c2cebb1bbc78d4183d6be6bcdd3805
Daniel Schatzmann <daniel.schatzmann@ost.ch>
2026-05-13 07:54:23 +0000

    fix: resolved pointer and returns smells

--- internal/controller/discovery/loaders.go
+++ internal/controller/discovery/loaders.go
@@ -9,16 +9,16 @@ import (
 )
 
 // NewLoader creates a loader by name
-func NewLoader(cfg core.CommonLoaderConfig, spec *gnmicv1alpha1.TargetSourceSpec) (core.Loader, core.CommonLoaderConfig, error) {
+func NewLoader(cfg *core.CommonLoaderConfig, spec gnmicv1alpha1.TargetSourceSpec) (core.Loader, error) {
 
 	switch {
 	case spec.Provider.HTTP != nil:
 		cfg.AcceptPush = spec.Provider.HTTP.AcceptPush
-		return http.New(cfg), cfg, nil
+		return http.New(*cfg), nil
 	case spec.Provider.Consul != nil:
-		return nil, cfg, fmt.Errorf("unknown targetsource loader, check TargetSource CRD for %s", cfg.TargetsourceNN)
+		return nil, fmt.Errorf("unknown targetsource loader, check TargetSource CRD for %s", cfg.TargetsourceNN)
 	default:
-		return nil, cfg, fmt.Errorf("unknown targetsource loader, check TargetSource CRD for %s", cfg.TargetsourceNN)
+		return nil, fmt.Errorf("unknown targetsource loader, check TargetSource CRD for %s", cfg.TargetsourceNN)
 	}
 
 }
--- internal/controller/targetsource_controller.go
+++ internal/controller/targetsource_controller.go
@@ -185,9 +185,7 @@ func (r *TargetSourceReconciler) startDiscovery(
 		targetSource,
 		targetChannel,
 	)
-	loader, loaderConfig, err := discovery.NewLoader(loaderConfig,
-		&targetSource.Spec,
-	)
+	loader, err := discovery.NewLoader(&loaderConfig, targetSource.Spec)
 	if err != nil {
 		logger.Error(err, "Target loader could not be created")
 		cleanup()
