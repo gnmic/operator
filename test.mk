76a620faeaf9c14cf995e1b848913bbceacdbd0a
Janooski <mail@romanweber.ch>
2026-06-03 12:52:44 +0000

    change url of curl

--- test.mk
+++ test.mk
@@ -100,7 +100,7 @@ send-target-to-apiserver:
 	@BEARER_TOKEN=$$(kubectl get secret gnmic-api-auth -n gnmic-system \
 		-o jsonpath='{.data.bearer-token}' | base64 --decode); \
 	kubectl port-forward -n gnmic-system svc/gnmic-controller-manager-api 8082:8082 --address=0.0.0.0 >/dev/null 2>&1 & \
-	curl -X POST "http://localhost:8082/api/v1/default/target-source/http-ts/applyTargets" \
+	curl -X POST "http://gnmic-controller-manager-api.gnmic-system.svc.cluster.local:8082/api/v1/default/target-source/http-ts/applyTargets" \
 		-H "Authorization: Bearer $$BEARER_TOKEN" \
 		-H "Content-Type: application/json" \
 		-d '[{"address":"clab-t1-leaf2","port":57400,"name":"leaf2","operation":"created","targetProfile":"default","labels":[{"key":"vendor","value":"nokia_srlinux"},{"key":"role","value":"leaf"}]}]'
